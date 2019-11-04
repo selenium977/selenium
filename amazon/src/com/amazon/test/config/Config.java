023514ba2234ef764b185d48f854c86e98e7733e
Oscar Raya <oraya@externosdeloittemx.com>
2019-11-04 12:28:12 -0600

    test

--- amazon/src/com/amazon/test/config/Config.java
+++ amazon/src/com/amazon/test/config/Config.java
@@ -2,6 +2,6 @@ package com.amazon.test.config;
 
 public class Config {
 //El raya se la come
-	private static String baseURL = "https://google.com.mx";
+	private static String baseURL = "https://google.com.mx";//comentario
 	
 }
