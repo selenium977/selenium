88d5c0f6fd42aeabaf254daa178d825de9cb6160
luistellez1294 <luitellez@DT665970.mx.deloitte.com>
2019-11-04 12:25:12 -0600

    Se agrega línea excepcional

--- amazon/src/com/amazon/test/config/Config.java
+++ amazon/src/com/amazon/test/config/Config.java
@@ -2,6 +2,8 @@ package com.amazon.test.config;
 
 public class Config {
 //El raya se la come
+	private boolean elRayaSeLaCome=true;
 	private static String baseURL = "https://google.com.mx";
+	
 	
 }
