2960b5070b12a4c7cfb28869694c4020b85b4ca3
Marina Santos <marinsantos@externosdeloittemx.com>
2019-11-04 15:37:43 -0600

    Formato para sidebarmenu

---
+++ amazon/src/com/amazon/header/elements/SideBarMenu.java
@@ -0,0 +1,51 @@
+package com.amazon.header.elements;
+
+import java.awt.Dimension;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class SideBarMenu {
+	
+	private static final By Menu				=By.xpath("//a[@id='nav-hamburger-menu']");
+	
+	//COMPRAR POR CATEGORIA 
+	/*private static final By AmazonPrimeVide    	=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By EchoYAlexa    		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By AmazonMusic 		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By AmazonFireTV   		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By EreadersEbookKindle =By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By AlimentosBebidas 	=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By Libros				=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By PelisSeriesTVMusic 	=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By Electronicos 		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By ComputoTablets		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By Software			=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By Videojuegos 		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By HogarCocina			=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By HerramientasHogar	=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By AutomotrizMotos		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By JuegosJuguetes		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By RopaZapatosAcces	=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By Handmade			=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By DeportesAireLibre	=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By SaludCuidadoPer		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By IndEmpresCienc		=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By TodasCategorias		=By.xpath("//a[@id='nav-hamburger-menu']");
+	
+	//AYUDA Y CONFIGURACIÓN 
+	private static final By MiCuenta			=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By Ayuda				=By.xpath("//a[@id='nav-hamburger-menu']");
+	private static final By Identificarse		=By.xpath("//a[@id='nav-hamburger-menu']");*/
+	
+}
