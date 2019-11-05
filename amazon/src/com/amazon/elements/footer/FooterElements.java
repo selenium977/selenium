c0f2e37ea7ebf7f6001fb376155da74bf3d51373
Oscar Raya <oraya@externosdeloittemx.com>
2019-11-05 12:02:37 -0600

    footer xpaths

--- amazon/src/com/amazon/elements/footer/FooterElements.java
+++ amazon/src/com/amazon/elements/footer/FooterElements.java
@@ -7,15 +7,45 @@ public class FooterElements {
 	
 	
 	
-	public static final By BACKTOP 		= By.xpath("//*[@id=\"navBackToTop\"]/div/span");
-	public static final By CONOCENOS 	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[1]/a");
-	public static final By INFO 		= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[2]/a");
-	//public static final By CONDITIONS 	= By.xpath("//*[@id=\"navFooter\"]/div[5]/ul[1]/li[1]/a");
-	public static final By PRESSDEPTH	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[3]/a");
-	public static final By AMAZONSELL	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[1]/a");
-	public static final By AMAZNSELLHAND= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[2]/a");
-	public static final By BOOKPUBLISH 	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[3]/a");
-	
-	
-	
+	public static final By BACKTOP 			= By.xpath("//*[@id=\"navBackToTop\"]/div/span");
+	//CONOCENOS
+	public static final By AMAZONJOBS 		= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[1]/a");
+	public static final By INFO 			= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[2]/a");
+	public static final By PRESSDEPTH		= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[3]/a");
+	//GANA DINERO CON NOSOTROS
+	public static final By AMAZONSELL		= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[1]/a");
+	public static final By AMAZNSELLHAND	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[2]/a");
+	public static final By KINDLE	 		= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[3]/a");
+	public static final By AFFILIATESPRO	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[4]/a");
+	public static final By PRODUCTSANNOUNCE = By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[5]/a");
+	//PODEMOS AYUDARTE
+	public static final By RETURNREPLACE	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[5]/ul/li[1]/a");
+	public static final By CONTENTANDDEVICES= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[5]/ul/li[2]/a");
+	public static final By HELP 			= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[5]/ul/li[3]/a");
+	//METODOS DE PAGO
+	public static final By CARDS			= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[1]/a");
+	public static final By GIFTCARDS		= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[2]/a");
+	public static final By MSI				= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[3]/a");
+	public static final By AMAZONCASH		= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[4]/a");
+	public static final By AMAZONRECHARGER	= By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[5]/a");
+	//COUNTRIES
+	public static final By AUSTRALIA		= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[1]/a");
+	public static final By GERMANY			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[2]/a");
+	public static final By BRASIL			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[3]/a");
+	public static final By CANADA			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[4]/a");
+	public static final By CHINA			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[5]/a");
+	public static final By SPAIN			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[6]/a");
+	public static final By USA				= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[7]/a");
+	public static final By FRANCE			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[8]/a");
+	public static final By INDIA			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[9]/a");
+	public static final By ITALY			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[10]/a");
+	public static final By JAPAN			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[11]/a");
+	public static final By NETHERLANDS		= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[12]/a");
+	public static final By UK				= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[13]/a");
+	public static final By SINGAPUR			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[14]/a");
+	public static final By TURKEY			= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[1]/li[15]/a");
+	//VARIOS
+	public static final By AWS				= By.xpath("//*[@id=\"navFooter\"]/div[4]/ul[2]/li/a");
+	public static final By USECONDITIONS	= By.xpath("//*[@id=\"navFooter\"]/div[5]/ul[1]/li[1]/a");
+	public static final By PRIVACY			= By.xpath("//*[@id=\"navFooter\"]/div[5]/ul[1]/li[2]/a");
 }
